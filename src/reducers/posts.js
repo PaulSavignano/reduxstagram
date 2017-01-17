7c6e456f29b0aa167e7978673e54d598310cf46a
Paul Savignano <Paul.Savignano@gmail.com>
2017-01-16 17:03:23 -0800

    Add posts reducer.

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,8 +1,17 @@
 // Reducer takes in action and a copy of state.
 
 function posts(state = [], action) {
-  console.log('The post will change');
-  console.log(state, action);
+  switch(action.type) {
+    case "INCREMENT_LIKES":
+      console.log("Incrementing Likes!");
+      const i = action.index;
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1),
+      ]
+    default: return state;
+  }
   return state;
 }
 
